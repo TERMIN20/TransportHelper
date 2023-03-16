4b666a21a5ec199049c795ab8be3a41b7af11170
TERMIN20 <58315733+TERMIN20@users.noreply.github.com>
2023-03-15 21:50:11 -0700

    Fake I did not add GeoJson Func, but I did make the initial camera move to Seattle :D

--- app/src/main/java/com/example/myapplication/MapActivity.kt
+++ app/src/main/java/com/example/myapplication/MapActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.layers.generated.circleLayer
 import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
@@ -23,24 +24,18 @@ class MapActivity : AppCompatActivity() {
         val initialCameraOptions = CameraOptions.Builder()
             .center(
                 Point.fromLngLat(
-                    47.6062,122.3321
+                    -122.335167,47.608013
                 )
             )
-            .zoom(15.5)
+            .zoom(11.0)
             .build()
 
         mapView?.getMapboxMap()?.setCamera(initialCameraOptions)
-        mapView?.getMapboxMap()?.getStyle{
-            style -> geoJsonSource("transit_stops") {
-                "asset://Transit_Stops_for_King_County_Metro___transitstop_point.geojson"}
-            circleLayer("transit_layer", "transit_stops"){
-            }
-        }
-
-
-
-
-
-
+//        mapView?.getMapboxMap()?.getStyle{
+//            style -> geoJsonSource("transit_stops") {
+//                "asset://Transit_Stops_for_King_County_Metro___transitstop_point.geojson"}
+//            circleLayer("transit_layer", "transit_stops"){
+//            }
+//        }
     }
 }
